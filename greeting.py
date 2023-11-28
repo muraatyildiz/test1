1e391d81364afc64c0445eb02a464ca6cefcd574
muraatyildiz <49946580+muraatyildiz@users.noreply.github.com>
2023-11-28 10:16:45 +0000

    Create greeting.py

---
+++ greeting.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+@app.route("/")#URL leading to method
+def hello(): # Name of the method
+ return("Hello World!")
+@app.route("/greetme")#different URL
+def helloall(): # different method name
+ name = request.args.get('name')#retrieve GET parameters
+ return("Hello {}!".format(name))#Python’s string.format
+if __name__ == "__main__":
+ app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
