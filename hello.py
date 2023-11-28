8474fd903466c8879720de98efb2297ac7993433
muraatyildiz <49946580+muraatyildiz@users.noreply.github.com>
2023-11-28 11:19:29 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def hello(): # Name of the method
  return("Hello World!second") #indent this line
 if __name__ == "__main__":
- app.run(host='0.0.0.0', port='8080', ssl_context=('cert.pem', 'privkey.pem')) # indent this line
+ app.run(host='0.0.0.0', port='8080') # indent this line , ssl_context=('cert.pem', 'privkey.pem')
