0eb3780a6c62901e882d6fdf68bc65f2b09946fc
muraatyildiz <49946580+muraatyildiz@users.noreply.github.com>
2023-11-28 12:42:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -73,5 +73,5 @@ def hello(): # Name of the method
   )
   return ret #Return the data in a string format
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080
-  app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
+  app.run(host='0.0.0.0',port='8081') #Run the flask app at port 8080
+  app.run(host='0.0.0.0',port='8081', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
